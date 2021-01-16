70559da00c9d30003c4887a358620d3862b303a5
Hosmar Colmenares <hosmar.colmenares@rappi.com>
2021-01-16 16:42:16 -0500

    add eslint

--- availability.js
+++ availability.js
@@ -1,78 +1,72 @@
-'use strict'
+const dayjs = require('dayjs');
 
-const dayjs = require('dayjs')
+function getDaysBetweenDates(fromDate, toDate) {
+  const days = {};
+  let currentDate = fromDate;
+  while (currentDate.isBefore(toDate)) {
+    currentDate = currentDate.add(1, 'days');
+    const weekday = currentDate.day();
+    if (days[weekday]) {
+      days[weekday] += 1;
+    } else {
+      days[weekday] = 1;
+    }
+  }
+
+  return days;
+}
+
+function getSecondsFromRange(ranges) {
+  return ranges.reduce((acc, elm) => {
+    if (elm[1] > elm[0]) {
+      return acc + (elm[1] - elm[0]);
+    }
+    return undefined;
+  }, 0);
+}
+
+function getAvailabilityPerDay(availabilityConfig) {
+  return availabilityConfig.reduce((acc, elm) => {
+    if (!elm.closed) {
+      if (elm.ranges.length) {
+        acc[elm.day] = getSecondsFromRange(elm.ranges);
+      } else {
+        acc[elm.day] = 86400;
+      }
+    } else {
+      acc[elm.day] = 0;
+    }
+
+    return acc;
+  }, {});
+}
+
+function validateDates(fromDate, toDate) {
+  const fromDateJs = dayjs(fromDate).isValid() ? dayjs(fromDate) : null;
+  const toDateJs = dayjs(toDate).isValid() ? dayjs(toDate) : null;
+  let error = null;
+
+  if (!fromDateJs || !toDateJs) {
+    error = 'Error: El formato de la fecha de entrada no es valido';
+  } else if (fromDateJs.isAfter(toDateJs) || toDateJs.diff(fromDateJs, 'd') < 1) {
+    error = 'Error: la fecha final debe ser mayor a la fecha inicial, y debe haber al menos un dia de diferencia.';
+  }
+
+  return { fromDateJs, toDateJs, error };
+}
 
 function getAvailableSeconds(fromDate, toDate, availabilityConfig) {
-    const { fromDateJs, toDateJs, error } = validateDates(fromDate, toDate)
-    if (error) {
-        return error
-    }
-    
-    const weekdaysBeetweenDates = getDaysBetweenDates(fromDateJs, toDateJs)
-    const availabilityPerDay = getAvailabilityPerDay(availabilityConfig)
-    const response = Object.keys(weekdaysBeetweenDates).reduce((acc, elm) => {
-        return acc + (weekdaysBeetweenDates[elm] * availabilityPerDay[elm]) 
-    }, 0)
+  const { fromDateJs, toDateJs, error } = validateDates(fromDate, toDate);
+  if (error) {
+    return error;
+  }
 
-    return !isNaN(response) ? response : 'Error: uno de los rangos esta mal definido' 
+  const weekdaysBeetweenDates = getDaysBetweenDates(fromDateJs, toDateJs);
+  const availabilityPerDay = getAvailabilityPerDay(availabilityConfig);
+  const response = Object.keys(weekdaysBeetweenDates)
+    .reduce((acc, elm) => acc + (weekdaysBeetweenDates[elm] * availabilityPerDay[elm]), 0);
+
+  return !Number.isNaN(response) ? response : 'Error: uno de los rangos esta mal definido';
 }
 
-function getDaysBetweenDates(fromDate, toDate){
-    const days = {}
-    let currentDate = fromDate
-    while (currentDate.isBefore(toDate)) {
-        currentDate = currentDate.add(1, 'days')
-        const weekday = currentDate.day()
-        if (days[weekday]) {
-            days[weekday]++
-        }else{
-            days[weekday] = 1
-        }
-    }
-
-    return days
-}
-
-function getAvailabilityPerDay(availabilityConfig){
-    return availabilityConfig.reduce((acc, elm) => {
-
-        if (!elm['closed']) {
-            if (elm['ranges'].length) {
-                acc[elm['day']] = getSecondsFromRange(elm['ranges'])
-            }else{
-                acc[elm['day']] = 86400
-            }
-        }else{
-            acc[elm['day']] = 0
-        }
-
-        return acc
-    },{})
-}
-
-function getSecondsFromRange(ranges){
-   return ranges.reduce((acc,elm) => {
-       if (elm[1] > elm[0]) {
-           return acc + (elm[1] - elm[0])
-       }else{
-           acc = undefined
-       }
-       
-    }, 0)
-}
-
-function validateDates(fromDate, toDate){
-    const fromDateJs = dayjs(fromDate).isValid() ? dayjs(fromDate) : null
-    const toDateJs = dayjs(toDate).isValid() ? dayjs(toDate) : null
-    let error = null
-
-    if (!fromDateJs || !toDateJs) {
-        error = 'Error: El formato de la fecha de entrada no es valido'
-    }else if (fromDateJs.isAfter(toDateJs) || toDateJs.diff(fromDateJs, 'd') < 1) {
-        error = 'Error: la fecha final debe ser mayor a la fecha inicial, y debe haber al menos un dia de diferencia.'
-    }
-
-    return { fromDateJs, toDateJs, error } 
-}
-
-module.exports = { getAvailableSeconds }
+module.exports = { getAvailableSeconds };
--- test/test.js
+++ test/test.js
@@ -1,284 +1,282 @@
-
-
 const assert = require('assert');
 const Availability = require('../availability');
 
-describe('Availability', function () {
-    describe('One day, available all day', function () {
-        it('should return 86400 seconds', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-01T00:00:00.000+0000'),
-                new Date('2021-01-02T00:00:00.000+0000'),
-                [
-                    {
-                        day: 5,
-                        ranges: []
-                    }
-                ]
-            )
-            assert.strictEqual(totalSeconds, 86400);
-        });
+describe('Availability', () => {
+  describe('One day, available all day', () => {
+    it('should return 86400 seconds', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-01T00:00:00.000+0000'),
+        new Date('2021-01-02T00:00:00.000+0000'),
+        [
+          {
+            day: 5,
+            ranges: [],
+          },
+        ],
+      );
+      assert.strictEqual(totalSeconds, 86400);
     });
+  });
 
-    describe('One day, available: 9hs to 18hs', function () {
-        it('should return 32400 seconds', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-01T00:00:00.000+0000'),
-                new Date('2021-01-02T00:00:00.000+0000'),
-                [
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]]
-                    }
-                ]
-            )
-            assert.strictEqual(totalSeconds, 32400);
-        });
+  describe('One day, available: 9hs to 18hs', () => {
+    it('should return 32400 seconds', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-01T00:00:00.000+0000'),
+        new Date('2021-01-02T00:00:00.000+0000'),
+        [
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+          },
+        ],
+      );
+      assert.strictEqual(totalSeconds, 32400);
     });
+  });
 
-    describe('One day, available: 9hs to 12hs, 14hs to 17hs', function () {
-        it('should return 21600 seconds', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-01T00:00:00.000+0000'),
-                new Date('2021-01-02T00:00:00.000+0000'),
-                [
-                    {
-                        day: 5,
-                        ranges: [
-                            [32400, 43200],
-                            [50400, 61200]
-                        ]
-                    }
-                ]
-            )
-            assert.strictEqual(totalSeconds, 21600);
-        });
+  describe('One day, available: 9hs to 12hs, 14hs to 17hs', () => {
+    it('should return 21600 seconds', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-01T00:00:00.000+0000'),
+        new Date('2021-01-02T00:00:00.000+0000'),
+        [
+          {
+            day: 5,
+            ranges: [
+              [32400, 43200],
+              [50400, 61200],
+            ],
+          },
+        ],
+      );
+      assert.strictEqual(totalSeconds, 21600);
     });
+  });
 
-    describe('One day, // closed', function () {
-        it('should return 0 seconds', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-01T00:00:00.000+0000'),
-                new Date('2021-01-02T00:00:00.000+0000'),
-                [
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]],
-                        closed: true 
-                    }
-                ]
+  describe('One day, // closed', () => {
+    it('should return 0 seconds', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-01T00:00:00.000+0000'),
+        new Date('2021-01-02T00:00:00.000+0000'),
+        [
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+            closed: true,
+          },
+        ],
 
-            )
-            assert.strictEqual(totalSeconds, 0);
-        });
+      );
+      assert.strictEqual(totalSeconds, 0);
     });
+  });
 
-    describe('One day, same date', function () {
-        it('should return error', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-01T00:00:00.000+0000'),
-                new Date('2021-01-01T00:00:00.000+0000'),
-                [
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]],
-                        closed: true
-                    }
-                ]
+  describe('One day, same date', () => {
+    it('should return error', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-01T00:00:00.000+0000'),
+        new Date('2021-01-01T00:00:00.000+0000'),
+        [
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+            closed: true,
+          },
+        ],
 
-            )
-            assert.strictEqual(totalSeconds, 'Error: la fecha final debe ser mayor a la fecha inicial, y debe haber al menos un dia de diferencia.');
-        });
+      );
+      assert.strictEqual(totalSeconds, 'Error: la fecha final debe ser mayor a la fecha inicial, y debe haber al menos un dia de diferencia.');
     });
+  });
 
-    describe('toDate before fromDate', function () {
-        it('should return error', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-02T00:00:00.000+0000'),
-                new Date('2021-01-01T00:00:00.000+0000'),
-                [
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]],
-                        closed: true
-                    }
-                ]
+  describe('toDate before fromDate', () => {
+    it('should return error', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-02T00:00:00.000+0000'),
+        new Date('2021-01-01T00:00:00.000+0000'),
+        [
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+            closed: true,
+          },
+        ],
 
-            )
-            assert.strictEqual(totalSeconds, 'Error: la fecha final debe ser mayor a la fecha inicial, y debe haber al menos un dia de diferencia.');
-        });
+      );
+      assert.strictEqual(totalSeconds, 'Error: la fecha final debe ser mayor a la fecha inicial, y debe haber al menos un dia de diferencia.');
     });
+  });
 
-    describe('less than one day', function () {
-        it('should return error', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-01T00:00:00.000+0000'),
-                new Date('2021-01-01T23:00:00.000+0000'),
-                [
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]],
-                        closed: true
-                    }
-                ]
+  describe('less than one day', () => {
+    it('should return error', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-01T00:00:00.000+0000'),
+        new Date('2021-01-01T23:00:00.000+0000'),
+        [
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+            closed: true,
+          },
+        ],
 
-            )
-            assert.strictEqual(totalSeconds, 'Error: la fecha final debe ser mayor a la fecha inicial, y debe haber al menos un dia de diferencia.');
-        });
+      );
+      assert.strictEqual(totalSeconds, 'Error: la fecha final debe ser mayor a la fecha inicial, y debe haber al menos un dia de diferencia.');
     });
+  });
 
-    describe('fromDate null', function () {
-        it('should return error', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                null,
-                new Date('2021-01-01T23:00:00.000+0000'),
-                [
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]],
-                        closed: true
-                    }
-                ]
+  describe('fromDate null', () => {
+    it('should return error', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        null,
+        new Date('2021-01-01T23:00:00.000+0000'),
+        [
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+            closed: true,
+          },
+        ],
 
-            )
-            assert.strictEqual(totalSeconds, 'Error: El formato de la fecha de entrada no es valido');
-        });
+      );
+      assert.strictEqual(totalSeconds, 'Error: El formato de la fecha de entrada no es valido');
     });
+  });
 
-    describe('toDate undefined', function () {
-        it('should return error', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-01T23:00:00.000+0000'),
-                null,
-                [
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]],
-                        closed: true
-                    }
-                ]
+  describe('toDate undefined', () => {
+    it('should return error', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-01T23:00:00.000+0000'),
+        null,
+        [
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+            closed: true,
+          },
+        ],
 
-            )
-            assert.strictEqual(totalSeconds, 'Error: El formato de la fecha de entrada no es valido');
-        });
+      );
+      assert.strictEqual(totalSeconds, 'Error: El formato de la fecha de entrada no es valido');
     });
+  });
 
-    describe('toDate invalid', function () {
-        it('should return error', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-01T23:00:00.000+0000'),
-                'bla',
-                [
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]],
-                        closed: true
-                    }
-                ]
+  describe('toDate invalid', () => {
+    it('should return error', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-01T23:00:00.000+0000'),
+        'bla',
+        [
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+            closed: true,
+          },
+        ],
 
-            )
-            assert.strictEqual(totalSeconds, 'Error: El formato de la fecha de entrada no es valido');
-        });
+      );
+      assert.strictEqual(totalSeconds, 'Error: El formato de la fecha de entrada no es valido');
     });
+  });
 
-    describe('wrong range', function () {
-        it('should return error', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-01T00:00:00.000+0000'),
-                new Date('2021-01-02T00:00:00.000+0000'),
-                [
-                    {
-                        day: 5,
-                        ranges: [[64800, 32400]]
-                    }
-                ]
+  describe('wrong range', () => {
+    it('should return error', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-01T00:00:00.000+0000'),
+        new Date('2021-01-02T00:00:00.000+0000'),
+        [
+          {
+            day: 5,
+            ranges: [[64800, 32400]],
+          },
+        ],
 
-            )
-            assert.strictEqual(totalSeconds, 'Error: uno de los rangos esta mal definido');
-        });
+      );
+      assert.strictEqual(totalSeconds, 'Error: uno de los rangos esta mal definido');
     });
+  });
 
-    describe('7 days, different availabilities', function () {
-        it('should return 162000 seconds', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-11T00:00:00.000+0000'),
-                new Date('2021-01-18T00:00:00.000+0000'),
-                [
-                    {
-                        day: 1,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 2,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 3,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 4,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 6,
-                        ranges: [],
-                        closed: true
-                    },
-                    {
-                        day: 0,
-                        ranges: [],
-                        closed: true
-                    }
-                ]
-            )
-            assert.strictEqual(totalSeconds, 162000);
-        });
+  describe('7 days, different availabilities', () => {
+    it('should return 162000 seconds', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-11T00:00:00.000+0000'),
+        new Date('2021-01-18T00:00:00.000+0000'),
+        [
+          {
+            day: 1,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 2,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 3,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 4,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 6,
+            ranges: [],
+            closed: true,
+          },
+          {
+            day: 0,
+            ranges: [],
+            closed: true,
+          },
+        ],
+      );
+      assert.strictEqual(totalSeconds, 162000);
     });
+  });
 
-    describe('10 days, different availabilities', function () {
-        it('should return 259200 seconds', function () {
-            const totalSeconds = Availability.getAvailableSeconds(
-                new Date('2021-01-11T00:00:00.000+0000'),
-                new Date('2021-01-21T00:00:00.000+0000'),
-                [
-                    {
-                        day: 1,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 2,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 3,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 4,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 5,
-                        ranges: [[32400, 64800]]
-                    },
-                    {
-                        day: 6,
-                        ranges: [],
-                        closed: true
-                    },
-                    {
-                        day: 0,
-                        ranges: [],
-                        closed: true
-                    }
-                ]
-            )
-            assert.strictEqual(totalSeconds, 259200);
-        });
+  describe('10 days, different availabilities', () => {
+    it('should return 259200 seconds', () => {
+      const totalSeconds = Availability.getAvailableSeconds(
+        new Date('2021-01-11T00:00:00.000+0000'),
+        new Date('2021-01-21T00:00:00.000+0000'),
+        [
+          {
+            day: 1,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 2,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 3,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 4,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 5,
+            ranges: [[32400, 64800]],
+          },
+          {
+            day: 6,
+            ranges: [],
+            closed: true,
+          },
+          {
+            day: 0,
+            ranges: [],
+            closed: true,
+          },
+        ],
+      );
+      assert.strictEqual(totalSeconds, 259200);
     });
+  });
 });
